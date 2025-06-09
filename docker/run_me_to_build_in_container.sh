b6323fbf60ea5ed50c8546e68a8600ffead3d58d
Fritz <fc@hedgecube.com>
2025-06-09 16:34:21 -0400

    docker update

--- docker/run_me_to_build_in_container.sh
+++ docker/run_me_to_build_in_container.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-SDIR=dirname $0
+SDIR=$(dirname $0)
 cd $SDIR
 cmake -DCMAKE_INSTALL_PREFIX=../install ../src
 make -j 10 && make install
 
-echo 'Build finished'
-echo "Binaries should be in $SDIR/../install/bin"
-echo "Now create your bentokey.bin file and copy it into $DB_KEYFILE location"
-echo "Or can use the following command to create it:"
-echo "echo your_key_here | ccrypt -e --key databento > $DB_KEYFILE"
-echo "Good luck! :)
+if [ $? -eq 0 ]; then
+    echo 'Build finished'
+    echo "Binaries should be in ${SDIR}/../install/bin"
+    echo "Now create your bentokey.bin file and copy it into \$DB_KEYFILE location ${DB_KEYFILE}"
+    echo "Or can use the following command to create it:"
+    echo "echo your_key_here | ccrypt -e --key databento > \$DB_KEYFILE"
+    echo 
+    echo "To fetch some data, try:"
+    echo "cd ..; ./install/bin/bentohistchains -s spy -n 3 -d 2025-05-02 -l info"
+    echo "Good luck! :)"
+else
+    echo "Build or install failed!"
+    exit 1
+fi
